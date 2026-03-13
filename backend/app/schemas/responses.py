3ae899cdaef53c486fc5fc60ab4b9296f3718afc
gcrespo <gcrespo1705@gmail.com>
2026-03-13 01:45:38 -0400

    added models for sheetname responses and requests

--- backend/app/schemas/responses.py
+++ backend/app/schemas/responses.py
@@ -4,3 +4,16 @@ from uuid import UUID
 class SessionCreation(BaseModel):
     session_id:UUID
     sheet_names:list[str]
+
+class SheetNameRequest(BaseModel):
+    session_id:UUID
+    req_payment_sheet:str
+    req_payment_index:int
+    recv_payment_sheet:str
+    recv_payment_index:int
+
+class SheetNameResponse(BaseModel):
+    req_payment_columns:list[str]
+    recv_payment_columns:list[str]
+    
+
