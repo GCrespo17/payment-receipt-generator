f1a90f4518373423eebb1329bca69cc5b08054e9
gcrespo <gcrespo1705@gmail.com>
2026-03-13 21:55:09 -0400

    created pdf data classes for header and payment data

---
+++ backend/app/models/pdf_data.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+@dataclass()
+class PDFHeader():
+    company_name:str
+    rif:str
+    doc_type:str
+    doc_number:str
+    sent_date:str
+    process_date:str
+
+class PDFPaymentData():
+    information_type:str
+    reference:str
+    ci_rif:str
+    name:str
+    account_number:str
+    amount:str
+    status:str
+
